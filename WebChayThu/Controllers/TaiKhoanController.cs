60e56a73e5fb4ee20362fe202c990f60a6d7a7c1
RawAndHie <ductath2008041@fpt.edu.vn>
2022-01-14 00:43:46 +0700

    Commit vscode

---
+++ WebChayThu/Controllers/TaiKhoanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebChayThu.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
